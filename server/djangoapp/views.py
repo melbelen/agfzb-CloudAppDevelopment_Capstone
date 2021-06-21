5bce038327600a380169bb0ae8033e2621fe7c33
Mel <rbbelen@gmail.com>
2021-06-21 21:45:12 +0000

    Updates

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -78,6 +78,16 @@ def registration_request(request):
             return render(request, 'djangoapp/user_registration_bootstrap.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
+def get_dealerships(request):
+    if request.method == "GET":
+        url = "your-cloud-function-domain/dealerships/dealer-get"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
+
 def get_dealer_reviews_from_cf(url, **kwargs):
     results = []
     # Call get_request with a URL parameter
